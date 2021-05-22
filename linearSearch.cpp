e691113656c37bc12144e85e03334d217ab4821e
desh7147-oss <57410836+desh7147-oss@users.noreply.github.com>
2021-05-22 12:07:05 +0530

    Add files via upload
    This is a rudimentary( or intuitive) searching algorithm "Linear Search".

---
+++ linearSearch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int search(int *A, int number, int size){
+ for(int i=0; i<size; ++i){
+
+ //the return 1 and 0 are exit status of the program
+ //where 0==successful  &&  1==failed
+ if(A[i]==number) return 0;
+ //notice there is no else used here bcz it will not allow us to search the whole array
+ }
+ //we've placed the return 1 after the for loop so that we can iterate and search the whole array successfully
+ return 1;
+}
+
+
+int main(void){
+int n;
+int Array[]={7,5,4,1,2};
+int size=sizeof(Array)/sizeof(Array[0]);
+cout<<"enter the number to search: ";
+cin>>n;
+if(search(Array, n, size)==0)
+cout<<"voila we found you";
+else
+cout<<"sorry you were not there";
+
+
+}
+
