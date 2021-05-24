80be3a1757f682848533a28c7f77c769e6124cb8
desh7147-oss <57410836+desh7147-oss@users.noreply.github.com>
2021-05-24 08:03:57 +0530

    Update linearSearch.cpp

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -6,7 +6,8 @@ int search(int *A, int number, int size){
  //the return 1 and 0 are exit status of the program
  //where 0==successful  &&  1==failed
  if(A[i]==number) return 0;
- //notice there is no else used here bcz it will not allow us to search the whole array
+ //notice there is no else used here bcz if we put an "else" here it will not allow us to search the whole array
+  // the abscence of "else" is saying "continue till the last element is reached or the desired element is found
  }
  //we've placed the return 1 after the for loop so that we can iterate and search the whole array successfully
  return 1;
